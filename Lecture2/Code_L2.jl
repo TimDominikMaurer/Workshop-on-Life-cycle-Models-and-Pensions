b85fa07026776448065a9c3b1c6c75b0f858185e
TimDominikMaurer <tim.maurer@alotria.ch>
2024-09-25 16:07:11 +0530

    Lecture 1-3 are done!

--- Lecture2/Code_L2.jl
+++ Lecture2/Code_L2.jl
@@ -1,8 +1,7 @@
 ###### 
 # Lecture 2: Structural Estimation
-using Pkg
-Pkg.add(["Plots", "Parameters", "Distributions", "LinearAlgebra", "Random", "Optim"])
-using Plots, Parameters, Distributions,LinearAlgebra,Random, Optim
+
+using Plots, Parameters, Distributions,LinearAlgebra, Optim
 
 # working directory is the current file
 cd(dirname(@__FILE__))
@@ -26,8 +25,6 @@ cd(dirname(@__FILE__))
     μ::Float64 = 0.0                                   # Location of Lognormal
     σ::Float64 = 1.0                                   # Scale of Lognormal
 	
-	# Simulation
-	Nsim::Int = 1000                                      # Number of simulated agents
 end
 
 # Create an instance of the struct
@@ -43,28 +40,33 @@ include("Functions_Lecture1.jl")
 # 2. The outcome is our "empirical" data set.
 # 3. We therefore know exactly what our estimation should lead to.
 
-# The true parameters are
+# The true parameter are
 para.β = 0.96
-para.ρ = 2.0
-# We simulate the model with the "true" parameters.
+# set the seed to replicaiton purposes
+Random.seed!(1)
+# We simulate the model with the "true" parameters
 ConsumptionData,SavingData = SimLCM(para,1000)
-# How many number of individuals are in the the data
+# How many of individuals are in the the data
 Ndata,_ = size(ConsumptionData)
 
 ############# 
 # Part C: Calculate the data moments
-ages = [5,10,15] # Ages to be targeted
-# Calculate moments of the data
-Λᵈ = mean(SavingData[:,ages],dims=1)
+# Ages to be targeted
+ages = [5,10,15]
+# Create moments of the data
+Λᵈ = mean(ConsumptionData[:,ages],dims=1)
 N_mom = length(Λᵈ) # number of moments
 
-mean(SavingData[:,ages],dims=1)
 ############# 
 # Part C: Wrtie a function that simulates the moments
-S = 100
-function sim_moments(S,ages,Ndata)
-	# This functions simulates the moments on S simulations
-    # Ndata: Number of individuals in the data
+S = 100 # set the number of simulations
+function sim_moments(S,ages,Ndata,para)
+    # Goal: This functions simulates the moments based on S simulations
+	# Inputs:
+	# S: Number of simulations
+	# ages: vector of ages to targeted
+    # Ndata: Number of individuals in the empirical data
+    # para: parameters of the model
     
     # Set the seed
     Random.seed!(1)
@@ -72,45 +74,50 @@ function sim_moments(S,ages,Ndata)
     Λˢₛ = zeros((S,length(ages)))
     for s in 1:S
         SimCon, SimSav = SimLCM(para,Ndata)
-        Λˢₛ[s,:] = mean(SimSav[:,ages],dims=1)
+        Λˢₛ[s,:] = mean(SimCon[:,ages],dims=1)
     end
     # stack them into a vector
     Λˢ = mean(Λˢₛ,dims=1)
 	return Λˢ
 end
-
-
-
-# write a fucntion for the difference
-function moments_diff(data,ages,S)
-	# This function solves for the
+############# 
+# Part D: Wrtie a function that calculates the error vector
+function moments_diff(data,ages,S,para)
+    # Goal: This function calculates the error vectors
+	# Inputs:
+	# data: empirical data
+	# ages: vector of ages to targeted
+    # S: Number of simulations
+    # para: parameters of the model
     
-    # Get the number of individuals in the data
-    Ndata,_ = size(data)
+    Ndata,_ = size(data) # Get the number of individuals in the empirical data
     # Data moments
-    # stack them into a vector
     Λᵈ= mean(data[:,ages],dims=1)
-    
-    # Simulate moments
-    Λˢ =  sim_moments(S,ages,Ndata)
-    
+    # Simulate moments based on parameters
+    Λˢ =  sim_moments(S,ages,Ndata,para)
+    # return the error vector
 	return vec(Λᵈ - Λˢ)
 end
+############# 
+# Part E: Wrtie a function for the objective function
+function smm_objective(Θ,data,ages,S,W,para)
+    # Goal: This function returns the objective function to be minimized
+	# Inputs:
+    # Θ: vector of parameters to be estimated
+	# data: empirical data
+	# ages: vector of ages to targeted
+    # S: Number of simulations
+    # W: Weighting matrix
+    # para: parameters of the model
 
-
-function smm_objective(Θ,data,ages,S,W)
-    
     # set parameter
     para.β = Θ[1]
-    # para.ρ = Θ[2]
-        
     # If W is not provided (i.e., is nothing), default to identity matrix
     if W === nothing
         W = Diagonal(ones(length(ages)))  # Identity matrix in Julia
     end
     # Compute the difference between data and model moments
-    Mdiff = moments_diff(data,ages,S)
-    
+    Mdiff = moments_diff(data,ages,S,para)
     # Compute the quadratic form
     objective_value = Mdiff' * W * Mdiff
     
@@ -118,132 +125,57 @@ function smm_objective(Θ,data,ages,S,W)
 end
 
 # Optimize using Nelder-Mead
-result = optimize(x -> smm_objective(x, SavingData,ages,S, W_d), [0.95], NelderMead())
+result = optimize(x -> smm_objective(x, ConsumptionData,ages,S, nothing,para), [0.90], NelderMead())
+# access the SSM estimator
+β_hat = Optim.minimizer(result)[1]
 
 # Print the result
-println("Estimated parameters: ", Optim.minimizer(result))
 println("Optimization details: ", result)
+println("Estimated parameter: ", β_hat)
 
 
-
-
-function smm_objective2(Θ,data,ages,S,W)
-    
-    # set parameter
-    para.β = Θ[1]
-    para.ρ = Θ[2]
-        
-    # If W is not provided (i.e., is nothing), default to identity matrix
-    if W === nothing
-        W = Diagonal(ones(length(ages)))  # Identity matrix in Julia
-    end
-    # Compute the difference between data and model moments
-    Mdiff = moments_diff(data,ages,S)
-    
-    # Compute the quadratic form
-    objective_value = Mdiff' * W * Mdiff
-    
-    return objective_value
+# Plot the objective function around the minimimum
+β_vec = collect(0.94:0.001:0.98)
+obj_vec = zeros(length(β_vec))
+for (i,β_i) in enumerate(β_vec)
+    obj_vec[i] = smm_objective([β_i], ConsumptionData,ages,S, nothing,para)
 end
-result = optimize(x -> smm_objective2(x, SavingData,ages,S, W_d), [0.95;2.2], NelderMead())
 
-# Print the result
-println("Estimated parameters: ", Optim.minimizer(result))
-println("Optimization details: ", result)
+# Plot 
+gr();
+plot(β_vec,obj_vec,label="Objective")
+vline!([0.96],label="True β")
+vline!([β_hat],label="Estimated β")
+plot!(xlabel="Guess of β", ylabel="Objective function")
+savefig("figtabs/Objective")
 
-smm_objective([0.95,2.5],SavingData,ages,S,W_d)
-para.β
-
-
-moments_diff(SavingData,ages,S)
-
-moments_diff(SavingData,ages,S)'*Diagonal([1.0, 1.0, 1.0])*moments_diff(SavingData,ages,S)
-
-
-para.ρ = Optim.minimizer(result)[1]
-Con1, A1 = solveLCM(para,0.0)
-
-# The true parameters are
-para.β = 0.98
-para.ρ = 2.0
-Con2, A2 = solveLCM(para,0)
-
-plot(Con1)
-plot!(Con2)
-plot(A1)
-plot!(A2)
-
-ages = [1,5,10,15,21]
-
-
-# Optimize using Nelder-Mead
-result = optimize(x -> smm_objective(x, SavingData,ages,S, nothing), [0.9], NelderMead())
-
-# Print the result
-println("Estimated parameters: ", Optim.minimizer(result))
-println("Optimization details: ", result)
-moments_diff(SavingData,ages,S)'*Diagonal(ones(length(ages)))*moments_diff(SavingData,ages,S)
-
-
-para.ρ = Optim.minimizer(result)[1]
-Con1, A1 = solveLCM(para,0.0)
-
-# The true parameters are
-para.β = 0.98
-para.ρ = 2.0
-Con2, A2 = solveLCM(para,0)
-
-plot(Con1)
-plot!(Con2)
-plot(A1)
-plot!(A2)
-
-ages = [1,5,8,10,12,15,21]
-
-
-# Optimize using Nelder-Mead
-result = optimize(x -> smm_objective(x, SavingData,ages,S, nothing), [0.9], NelderMead(),tol=0.0000001)
-
-optimize()
-# Print the result
-println("Estimated parameters: ", Optim.minimizer(result))
-println("Optimization details: ", result)
-moments_diff(SavingData,ages,S)'*Diagonal(ones(length(ages)))*moments_diff(SavingData,ages,S)
-
-
-para.ρ = Optim.minimizer(result)[1]
-Con1, A1 = solveLCM(para,0.0)
-
-# The true parameters are
-para.β = 0.98
-para.ρ = 2.0
-Con2, A2 = solveLCM(para,0)
-
-plot(Con1)
-plot!(Con2)
-plot(A1)
-plot!(A2)
-
-
-
+#############
+# Optional Part: Estimation of variance of estimator
 
 # Bootstrap the data with replacement
 B = 1000 # Number of bootstrap replications
 # To store bootstrap results
 bootstrap_moments = zeros(B,N_mom)
-
-rand(1:Ndata,Ndata)
 # Perform bootstrapping
 for b in 1:B
-    # Sample with replacement for each age group
     for (i,age) in enumerate(ages)
-    sample = SavingData[rand(1:Ndata, Ndata),age]
-    
+    # Sample with replacement for each age group
+    sample = ConsumptionData[rand(1:Ndata, Ndata),age]
     # Calculate mean consumption for each age in the bootstrap sample
     bootstrap_moments[b, i] = mean(sample)
     end
 end
+# Calculate the bootstrapped variance-covariance matrix of the moments in the data
+Ω_hat = cov(bootstrap_moments)
 
-cov_moments = cov(bootstrap_moments)
-var_moments = diag(cov_moments)
-W_d = diagm(var_moments.^-1)
+# approximate Jacobian
+h = 0.001
+para.β = β_hat - h
+M_h_minus = sim_moments(S,ages,Ndata,para)
+para.β = β_hat + h
+M_h_plus = sim_moments(S,ages,Ndata,para)
+G_hat = - vec((M_h_minus-M_h_plus)/(2*h))
+
+# Now, V is the computed variance-covariance matrix
+V_hat = inv(G_hat' * W * G_hat) * G_hat' * W * Ω_hat * W' * G_hat * inv(G_hat' * W * G_hat)
+
